61e6099f240c1ec5af3741a8077a0e64ffacbc54
masterwifinetworksolution <myarachma92@gmail.com>
2024-11-29 00:06:50 +0800

    update commit

--- assets/js/anti-adblock.js
+++ assets/js/anti-adblock.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-<<<<<<< HEAD
     var adblockDetected = false;
     
     // Elemen yang umumnya diblokir oleh adblocker
@@ -12,26 +11,13 @@ document.addEventListener('DOMContentLoaded', function () {
     window.setTimeout(function() {
         if (ad.offsetHeight === 0) {
             adblockDetected = true;
+            document.getElementById('content').style.display = 'block';
             document.body.innerHTML = '<div style="text-align: center; margin-top: 50px;"><h1>Please disable your adblocker to view this site.</h1></div>';
         } else {
             adblockDetected = false;
-            document.getElementById('content').style.display = 'block';
+            document.getElementById('content').style.display = 'true';
         }
         document.body.removeChild(ad);
     }, 100);
-=======
-    function checkAdblock() {
-        if (typeof adblock === 'undefined') {
-            document.body.innerHTML = '<div style="text-align: center; margin-top: 50px;"><h1>Please disable your adblocker to view this site.</h1></div>';
-        } else {
-            location.reload();
-        }
-    }
-    
-    checkAdblock();
-    
-    // Cek lagi setelah beberapa detik, untuk melihat apakah adblocker sudah dinonaktifkan
-    setInterval(checkAdblock, 3000);
->>>>>>> origin/main
 });
 
