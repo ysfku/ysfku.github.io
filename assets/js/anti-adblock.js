5a9311a2fb7f7ce5c01d92d1973672f062021689
masterwifinetworksolution <myarachma92@gmail.com>
2024-11-28 22:38:33 +0800

    update commit

--- assets/js/anti-adblock.js
+++ assets/js/anti-adblock.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    if (typeof adblock === 'undefined') {
-        document.body.innerHTML = '<div style="text-align: center; margin-top: 50px;"><h1>Please disable your adblocker to view this site.</h1></div>';
+    function checkAdblock() {
+        if (typeof adblock === 'undefined') {
+            document.body.innerHTML = '<div style="text-align: center; margin-top: 50px;"><h1>Please disable your adblocker to view this site.</h1></div>';
+        } else {
+            location.reload();
+        }
     }
+    
+    checkAdblock();
+    
+    // Cek lagi setelah beberapa detik, untuk melihat apakah adblocker sudah dinonaktifkan
+    setInterval(checkAdblock, 3000);
 });
