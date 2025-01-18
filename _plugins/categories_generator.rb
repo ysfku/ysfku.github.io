a128a747ceb05faa2c8a203d446501c3938a105d
masterwifinetworksolution <myarachma92@gmail.com>
2025-01-19 04:13:10 +0800

    update commit

--- _plugins/categories_generator.rb
+++ _plugins/categories_generator.rb
@@ -1,16 +1,37 @@
 module Jekyll
+  class CategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category.html')
+      self.data['category'] = category
+      self.data['title'] = category.capitalize
+    end
+  end
+
   class CategoryGenerator < Generator
     safe true
 
     def generate(site)
-      categories = []
-      Dir.foreach("categories") do |category|
-        next if category == '.' or category == '..'
-        category_path = "/categories/#{category}"
-        post_count = Dir[File.join("categories/#{category}", "**", "*")].count { |file| File.file?(file) }
-        categories << { "name" => category.capitalize, "url" => category_path, "posts" => post_count }
+      if site.layouts.key? 'category'
+        dir = site.config['category_dir'] || 'categories'
+        categories = site.posts.docs.flat_map { |post| post['categories'] }.uniq
+        categories.each do |category|
+          puts "Generating category page for #{category}" # Log tambahan
+          write_category_page(site, File.join(dir, category), category)
+        end
       end
-      site.config['categories'] = categories
+    end
+
+    def write_category_page(site, dir, category)
+      index = CategoryPage.new(site, site.source, dir, category)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
     end
   end
 end
--- categories/clash-for-magisk.md
+++
@@ -1,4 +0,0 @@
----
-layout: category
-title: clash for magisk
----
